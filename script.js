24191e1a5b76f8416aff70b0e3fe03e63e20bfc2
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-03-17 17:12:25 -0500

    added comments to JS and HTML

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let currentHumidity;
 // Grab the container for search history buttons
 const searchHistoryContainer = document.querySelector('#search-history-container');
 
+// Function to pull lattitude and longitude from the weather API
 const getCoordinates = function() {
     let searchCity = document.querySelector('#search-bar').value;
     console.log(searchCity);
@@ -27,20 +28,21 @@ const getCoordinates = function() {
             let cityLat = data[0].lat;
             let cityLon = data[0].lon;
             
-            // Add searched city to search history container
+            // Add the searched city to the search history container
             const searchHistoryButton = document.createElement('button');
-            searchHistoryButton.setAttribute('class', 'btn btn-secondary mt-3 col-12 mx-auto')
+            searchHistoryButton.setAttribute('class', 'btn btn-secondary mt-3 col-12 mx-auto');
             searchHistoryButton.textContent = searchCity;
             searchHistoryButton.addEventListener('click', function() {
                 weatherCondition(cityLat, cityLon);
             });
-
             searchHistoryContainer.appendChild(searchHistoryButton);
             weatherCondition(cityLat, cityLon);
         })
 };
 
+// Function to pull weather conditions using the lat and lon parameters
 const weatherCondition = function(cityLat, cityLon) {
+
     // Clear any previous search data displayed
     let forecastContainer = document.getElementById('forecast-container');
     forecastContainer.innerHTML = '';
@@ -64,7 +66,7 @@ const weatherCondition = function(cityLat, cityLon) {
         console.log(data.list[0].main.humidity);
         displayWeather(cityName, currentTemp, currentWindSpeed, currentHumidity);
 
-        // Display 5-day forecast
+        // Loop to pull data for current city every 24 hours
         for (let i = 1; i <=40 ; i+=8) {
             let forecastDate = dayjs(data.list[i].dt_txt).format("MM/DD/YYYY");
             let forecastCondition = data.list[i].weather[0].description;
@@ -78,10 +80,10 @@ const weatherCondition = function(cityLat, cityLon) {
 
             // Create HTML elements to display forecast
             let forecastDiv = document.createElement('div');
-            forecastDiv.classList.add('col-sm-1', 'align-items-center', 'forecast-card', 'card', 'bg-primary');
+            forecastDiv.classList.add('col-sm-1', 'align-items-center', 'forecast-card', 'card', 'bg-primary', 'mb-3');
 
             let forecastDateEl = document.createElement('h5');
-            forecastDateEl.classList.add('mt-3', 'mb-0')
+            forecastDateEl.classList.add('mt-3', 'mb-0');
             forecastDateEl.textContent = forecastDate;
 
             let forecastIconEl = document.createElement('img');
@@ -111,16 +113,16 @@ const weatherCondition = function(cityLat, cityLon) {
             forecastDiv.appendChild(forecastWindEl);
 
             document.querySelector('#forecast-container').appendChild(forecastDiv);
-        }
+        };
     });
 };
 
-
-// Display the current weather conditions for the city, including the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed.
+// Display the current weather conditions for the city
 const displayWeather = function(cityName, currentTemp, currentWindSpeed, currentHumidity) {
     document.querySelector('#current-icon').setAttribute('src', currentIconUrl);
-    document.getElementById('current-icon').setAttribute('style', 'width: 10rem; height: 10rem')
+    document.getElementById('current-icon').setAttribute('style', 'width: 10rem; height: 10rem');
     document.querySelector('#city-name').textContent = cityName + ' (' + currentDate + ')';
+
     // document.querySelector('#current-date').textContent = ;
     document.querySelector('#current-condition').textContent = currentCondition;
     document.querySelector('#current-temp').textContent = "Temp: " + currentTemp + " °F";
@@ -131,21 +133,3 @@ const displayWeather = function(cityName, currentTemp, currentWindSpeed, current
 submitButton.addEventListener("click", function(e){
     getCoordinates();
 });
-
-
-
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// Display the future weather conditions for the city in a 5-day forecast, including the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity.
-//using dayjs for date info
-
-
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-// Add the searched city to the search history, so that users can easily click on it to view its weather conditions again.
-// When a city in the search history is clicked, fetch its current and future weather data using the API, and display it on the dashboard as described in steps 3 and 4.
-
-// Create an empty array for search history
-// let searchHistory = [];
-
-    // Create a function to  
